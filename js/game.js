21477d592b2c06e24aca09e632cb3c396f3988ea
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-19 18:10:55 +0000

    gamejs

--- js/game.js
+++ js/game.js
@@ -5,6 +5,12 @@
 // Created on: Apr 2024
 // This is the Phaser3 game configuration file
 
+// scene import statements
+import SplashScene from "./splashScene"
+
+// create the new scenes
+const splashScene = new SplashScene()
+
 /**
  * Start Phaser Game.
  */
@@ -28,4 +34,11 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
-console.log(game)
+// console.log(game)
+
+// load scenes
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+
+// the start scene
+game.scene.start("splashScene")
