5abfb94fb226608ff30337bd64238cfbf98f7c88
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-29 18:38:54 +0000

    menu

--- js/game.js
+++ js/game.js
@@ -1,19 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+/* global phaser */
+
+// Copyright (c) 2024 Fetuha All rights reserved
 //
 // Created by: Fetuha
-// Created on: April 2024
-// This is the Phaser3 configuration file
+// Created on: Apr 2024
+// This is the Phaser3 game configuration file
 
 // scene import statements
-import SplashScene from './splashScene.js'
+import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
-
-// create new scenes
+import MenuScene from "./menuScene.js"
+//create the new scenes
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
-
+const menuScene = new MenuScene()
 /**
- * Start Phaser Game.
+ * Start Phaser Game
  */
 const config = {
   type: Phaser.AUTO,
@@ -25,22 +27,21 @@ const config = {
       debug: true,
     },
   },
-  // set background colour
-  backgroundColor: 0xffffff,
+  // Background colour
+  backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
 
 const game = new Phaser.Game(config)
-//console.log(game)
+// console.log(game)
 
-// load scenes
-// NOTE: remember any "key" is global and CAN NOT be reused!
-game.scene.add('splashScene', splashScene)
-game.scene.add('titleScene', titleScene)
-
+// load notes
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
 // the start scene
-game.scene.start('splashScene')
+game.scene.start("splashScene")
