0ad8ee1cdf4c45ea5f766044ebb5f462d5845058
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-03 18:29:17 +0000

    game sceene

--- js/gameScene.js
+++ js/gameScene.js
@@ -33,6 +33,9 @@ class GameScene extends Phaser.Scene {
    */
   preload() {
     console.log("Game Scene")
+
+    this.load.image("startBackground", "./assets/startBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
   }
 
   /**
@@ -41,7 +44,10 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
+    this.background.set0rgin(0, 0)
+
+    this.ship = this,physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
 
   /**
