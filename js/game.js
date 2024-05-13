36dddbf75e3ba0b7d5351d6e4f1a778ac3211b38
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-13 18:03:48 +0000

    js

--- js/game.js
+++ js/game.js
@@ -1,20 +1,16 @@
 /* global Phaser */
 
-// Created by: Fetuha
+// Created by:Fetuha
 //
 // Created on: Apr 2024
 // This is the Phaser3 game configuration file
 
 // scene import statements
 import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
-import GameScene from "./gameScene.js"
+
 //create the new scenes
 const splashScene = new SplashScene()
-const titleScene = new TitleScene()
-const menuScene = new MenuScene()
-const gameScene = new GameScene()
+
 /**
 * Start Phaser Game
 */
@@ -42,8 +38,6 @@ const game = new Phaser.Game(config)
 // load notes
 // Note: remember any "key" is global and CAN NOT be reused!
 game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
-game.scene.add("gameScene", gameScene)
+
 // the start scene
 game.scene.start("splashScene")
--- js/splashScene.js
+++ js/splashScene.js
@@ -1,68 +1,57 @@
-/* global Phaser */
+/* global phaser */
 
 // Copyright (c) 2024 Fetuha All rights reserved
 //
-// Created by:Fetuha
+// Created by: Fetuha
 // Created on: Apr 2024
-// This is the Phaser3 game configuration file
+// This is the Splash Scene
 
 /**
- * This class is the Splash Scene.
- */
+* This class is the Splash Scene
+*/
 class SplashScene extends Phaser.Scene {
   /**
-   * This method is the constructor.
+   * This method is the constructor
    */
   constructor() {
     super({ key: "splashScene" })
-    this.SplashSceneBackgroundImage = null
   }
 
   /**
-   * Can be defined on your own Scenes. 
-   * This method is called by the Scene Manager when the scene starts, 
-   *    before preload() and create().
-   *  @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   * Can be defined on your own Scenes
+   * This method is called by the Scene Manager when the gstarts,
+   * before preload() and create()
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("ffffff")
   }
 
   /**
-   * Can be defined on your own Scene.
-   * Use it to load assets.
+   * Can be defined on your own Scenes
+   * Use it to load assets
    */
   preload() {
     console.log("Splash Scene")
-    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   /**
-   * Can be defied on your own Scene. 
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   * Can be defined on your own Scenes
+   * Use it to create your game objects
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   create(data) {
-    this.SplashSceneBackgroundImage = this.add.sprite(
-      0,
-      0,
-      "splashSceneBackground"
-    )
-    this.SplashSceneBackgroundImage.x = 1920 / 2
-    this.SplashSceneBackgroundImage.y = 1080 / 2
     // pass
   }
 
   /**
-   * Should be overridden by your own Scenes.
-   * This method is called onces per game step while the scene is running.
-   * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
+   * Should be overridden by your own Scenes
+   * This method is called once per game step while the scene is running
+   * @param {number} time - The current time
+   * @param {number} delta - The delta time in ms since the last frame
    */
   update(time, delta) {
-    if (time > 3000) {
-      this.scene.switch("titleScene")
-    }
+    // pass
   }
 }
 
